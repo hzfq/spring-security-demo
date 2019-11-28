e6fb400a88ea44126c43a25be293e5ac1fce287a
hzfq <1443754157@qq.com>
2019-11-28 17:32:55 +0800

    update email

--- spring-security-jpa-demo/src/main/java/top/hzfq/security/jpa/Application.java
+++ spring-security-jpa-demo/src/main/java/top/hzfq/security/jpa/Application.java
@@ -3,6 +3,11 @@ package top.hzfq.security.jpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author hzfq
+ * @email huzhifengqing@qq.com
+ * @date 2019/11/28
+ */
 @SpringBootApplication
 public class Application {
 
---
+++ spring-security-jpa-demo/src/main/java/top/hzfq/security/jpa/domain/FKRole.java
@@ -0,0 +1,9 @@
+package top.hzfq.security.jpa.domain;
+
+/**
+ * @author hzfq
+ * @email huzhifengqing@qq.com
+ * @date 2019/11/28
+ */
+public class FKRole {
+}
